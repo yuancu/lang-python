abdbd9b931c74d2a373ed50d453774171dfd171d
Yuanchun Shen <yuanchu@amazon.com>
2025-01-08 19:45:21 -0800

    Initiate a scaffold for the lang-pyhton plugin
    - passed integration test

--- src/main/java/org/opensearch/python/PythonModulePlugin.java
+++ src/main/java/org/opensearch/python/PythonModulePlugin.java
@@ -5,11 +5,12 @@
  * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
-package org.example.path.to.plugin;
+package org.opensearch.python;
 
 import org.opensearch.plugins.Plugin;
+import org.opensearch.plugins.ScriptPlugin;
 
 
-public class RenamePlugin extends Plugin {
+public class PythonModulePlugin extends Plugin implements ScriptPlugin{
     // Implement the relevant Plugin Interfaces here
 }
--- src/test/java/org/opensearch/python/PythonModuleTests.java
+++ src/test/java/org/opensearch/python/PythonModuleTests.java
@@ -5,10 +5,10 @@
  * this file be licensed under the Apache-2.0 license or a
  * compatible open source license.
  */
-package org.example.path.to.plugin;
+package org.opensearch.python;
 
 import org.opensearch.test.OpenSearchTestCase;
 
-public class RenameTests extends OpenSearchTestCase {
+public class PythonModuleTests extends OpenSearchTestCase {
     // Add unit tests for your plugin
 }
