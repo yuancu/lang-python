c0cd16ed09158155ec5ee3ee2b9bf256f16139d6
Yuanchun Shen <yuanchu@amazon.com>
2025-04-10 18:13:11 +0800

    Apply spotless

--- src/main/java/org/opensearch/python/PythonFieldScript.java
+++ src/main/java/org/opensearch/python/PythonFieldScript.java
@@ -1,13 +1,12 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
  */
 
 package org.opensearch.python;
 
+import java.io.IOException;
+import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.index.LeafReaderContext;
@@ -17,39 +16,39 @@ import org.graalvm.polyglot.Value;
 import org.opensearch.script.FieldScript;
 import org.opensearch.search.lookup.SearchLookup;
 
-import java.io.IOException;
-import java.util.Map;
-
 public class PythonFieldScript {
     private static final Logger logger = LogManager.getLogger();
 
-    public static FieldScript.Factory newFieldScriptFactory(String code){
-        return new FieldScript.Factory(){
+    public static FieldScript.Factory newFieldScriptFactory(String code) {
+        return new FieldScript.Factory() {
             @Override
             public boolean isResultDeterministic() {
                 return true;
             }
 
             @Override
-            public FieldScript.LeafFactory newFactory(Map<String, Object> params, SearchLookup lookup) {
+            public FieldScript.LeafFactory newFactory(
+                    Map<String, Object> params, SearchLookup lookup) {
                 return newFieldScript(code, params, lookup);
             }
         };
     }
 
-    private static  FieldScript.LeafFactory newFieldScript(String code, Map<String, Object> params, SearchLookup lookup){
+    private static FieldScript.LeafFactory newFieldScript(
+            String code, Map<String, Object> params, SearchLookup lookup) {
         logger.info("Executing python code: {}", code);
         logger.info("Params: {}", params.toString());
         logger.info("Lookup: {}", lookup.toString());
         return new PythonFieldScriptLeafFactory(code, params, lookup);
     }
 
-    private static class PythonFieldScriptLeafFactory implements  FieldScript.LeafFactory {
+    private static class PythonFieldScriptLeafFactory implements FieldScript.LeafFactory {
         private final String code;
         private Map<String, Object> params;
         private SearchLookup lookup;
 
-        private PythonFieldScriptLeafFactory(String code, Map<String, Object> params, SearchLookup lookup) {
+        private PythonFieldScriptLeafFactory(
+                String code, Map<String, Object> params, SearchLookup lookup) {
             this.code = code;
             this.params = params;
             this.lookup = lookup;
@@ -67,10 +66,12 @@ public class PythonFieldScript {
         }
     }
 
-    private static Void runPython(String code){
-        try (Context context = Context.newBuilder("python")
-            .sandbox(SandboxPolicy.TRUSTED)
-            .allowAllAccess(false).build()) {
+    private static Void runPython(String code) {
+        try (Context context =
+                Context.newBuilder("python")
+                        .sandbox(SandboxPolicy.TRUSTED)
+                        .allowAllAccess(false)
+                        .build()) {
 
             // TODO: check whether code has a field `result`
             context.eval("python", code);
@@ -78,7 +79,7 @@ public class PythonFieldScript {
             Value result = context.getBindings("python").getMember("result");
 
             logger.info("Result {}", result.asInt());
-        } catch (Exception e){
+        } catch (Exception e) {
             logger.error("Failed to run python code", e);
         }
         return null;
--- src/main/java/org/opensearch/python/PythonScoreScript.java
+++ src/main/java/org/opensearch/python/PythonScoreScript.java
@@ -1,33 +1,23 @@
 /*
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
  */
 
 package org.opensearch.python;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.search.IndexSearcher;
-import org.graalvm.polyglot.HostAccess;
-import org.graalvm.polyglot.Value;
-import org.opensearch.script.GeneralScriptException;
-import org.opensearch.script.ScoreScript;
-import org.opensearch.search.lookup.SearchLookup;
-
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.SandboxPolicy;
-
-import java.security.AccessController;
 import java.io.IOException;
+import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.search.IndexSearcher;
+import org.graalvm.polyglot.Value;
+import org.opensearch.script.ScoreScript;
+import org.opensearch.search.lookup.SearchLookup;
 
 // Reference memo (For developers only)
 // - https://www.elastic.co/guide/en/elasticsearch/painless/current/painless-score-context.html
@@ -35,8 +25,8 @@ import java.util.Set;
 public class PythonScoreScript {
     private static final Logger logger = LogManager.getLogger();
 
-    public static ScoreScript.Factory newScoreScriptFactory(String code){
-        return new ScoreScript.Factory(){
+    public static ScoreScript.Factory newScoreScriptFactory(String code) {
+        return new ScoreScript.Factory() {
 
             @Override
             public boolean isResultDeterministic() {
@@ -44,14 +34,19 @@ public class PythonScoreScript {
             }
 
             @Override
-            public ScoreScript.LeafFactory newFactory(Map<String, Object> params, SearchLookup lookup, IndexSearcher indexSearcher) {
+            public ScoreScript.LeafFactory newFactory(
+                    Map<String, Object> params, SearchLookup lookup, IndexSearcher indexSearcher) {
                 return newScoreScript(code, params, lookup, indexSearcher);
             }
         };
     }
 
-    private static ScoreScript.LeafFactory newScoreScript(String code, Map<String, Object> params, SearchLookup lookup, IndexSearcher indexSearcher) {
-        return  new PythonScoreScriptLeafFactory(code, params, lookup, indexSearcher);
+    private static ScoreScript.LeafFactory newScoreScript(
+            String code,
+            Map<String, Object> params,
+            SearchLookup lookup,
+            IndexSearcher indexSearcher) {
+        return new PythonScoreScriptLeafFactory(code, params, lookup, indexSearcher);
     }
 
     private static class PythonScoreScriptLeafFactory implements ScoreScript.LeafFactory {
@@ -60,7 +55,11 @@ public class PythonScoreScript {
         private final SearchLookup lookup;
         private final IndexSearcher indexSearcher;
 
-        private PythonScoreScriptLeafFactory(String code, Map<String, Object> params, SearchLookup lookup, IndexSearcher indexSearcher) {
+        private PythonScoreScriptLeafFactory(
+                String code,
+                Map<String, Object> params,
+                SearchLookup lookup,
+                IndexSearcher indexSearcher) {
             this.code = code;
             this.params = params;
             this.lookup = lookup;
@@ -78,28 +77,37 @@ public class PythonScoreScript {
             return new ScoreScript(params, lookup, indexSearcher, ctx) {
                 @Override
                 public double execute(ExplanationHolder explanation) {
-                    if(explanation != null) {
-                        explanation.set("Use user-provided Python expression to calculate the score of the document");
+                    if (explanation != null) {
+                        explanation.set(
+                                "Use user-provided Python expression to calculate the score of the"
+                                        + " document");
                     }
 
-                    logger.info("Code is expression: {}", PythonScriptUtility.isCodeAnExpression(code));
-//                    if (!PythonScriptUtility.isCodeAnExpression(code)) {
-//                        //TODO: Convert to ScriptException @see ExpressionScriptEngine.java#L444
-//                        throw new GeneralScriptException("Python score script must be an expression, but got " + code);
-//                    }
+                    logger.info(
+                            "Code is expression: {}", PythonScriptUtility.isCodeAnExpression(code));
+                    //                    if (!PythonScriptUtility.isCodeAnExpression(code)) {
+                    //                        //TODO: Convert to ScriptException @see
+                    // ExpressionScriptEngine.java#L444
+                    //                        throw new GeneralScriptException("Python score script
+                    // must be an expression, but got " + code);
+                    //                    }
 
-                    Set<String> accessedDocFields = PythonScriptUtility.extractAccessedDocFields(code);
+                    Set<String> accessedDocFields =
+                            PythonScriptUtility.extractAccessedDocFields(code);
                     Map<String, Object> docParams = new HashMap<>();
                     for (String field : accessedDocFields) {
                         docParams.put(field, getDoc().get(field));
                     }
 
-                    return AccessController.doPrivileged((PrivilegedAction<Double>) () -> executePython(code, params, docParams, get_score()));
+                    return AccessController.doPrivileged(
+                            (PrivilegedAction<Double>)
+                                    () -> executePython(code, params, docParams, get_score()));
                 }
             };
         }
 
-        private static double executePython(String code, Map<String, ?> params, Map<String, ?> doc, double score){
+        private static double executePython(
+                String code, Map<String, ?> params, Map<String, ?> doc, double score) {
             Value evaluatedVal = ExecutionUtils.executePython(code, params, doc, score);
             if (evaluatedVal == null) {
                 return 0;
