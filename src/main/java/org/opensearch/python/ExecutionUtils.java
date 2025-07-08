44ea1490317edfd5d19cef13104b9a6c65f1da99
Yuanchun Shen <yuanchu@amazon.com>
2025-07-08 17:10:55 +0800

    Add basic timeout to python execution
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- src/main/java/org/opensearch/python/ExecutionUtils.java
+++ src/main/java/org/opensearch/python/ExecutionUtils.java
@@ -5,7 +5,15 @@
 
 package org.opensearch.python;
 
+import java.time.Duration;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.graalvm.polyglot.Context;
@@ -16,36 +24,51 @@ import org.graalvm.python.embedding.GraalPyResources;
 
 public class ExecutionUtils {
     private static final Logger logger = LogManager.getLogger();
+    @Getter @Setter private static int TIMEOUT_IN_SECONDS = 10;
+
+    private static Value executeWorker(
+            Context context, String code, Map<String, ?> params, Map<String, ?> doc, Double score) {
+        if (params != null) {
+            logger.debug("Params: {}", params.toString());
+            context.getBindings("python").putMember("params", params);
+        }
+        if (doc != null) {
+            logger.debug("Doc: {}", doc.toString());
+            context.getBindings("python").putMember("doc", doc);
+        }
+        if (score != null) {
+            logger.debug("Score: {}", score);
+            context.getBindings("python").putMember("_score", score);
+        }
+        return context.eval("python", code);
+    }
 
     public static Value executePython(
             String code, Map<String, ?> params, Map<String, ?> doc, Double score) {
-        // A working context without capabilities to import packages:
-        // Context context = Context.newBuilder("python")
-        //            .sandbox(SandboxPolicy.TRUSTED)
-        //            .allowHostAccess(HostAccess.ALL).build()
-        try (Context context =
-                GraalPyResources.contextBuilder()
-                        .sandbox(SandboxPolicy.TRUSTED)
-                        .allowHostAccess(HostAccess.ALL)
-                        // The following 2 options are necessary for importing 3-rd party libraries
-                        // that load native libraries
-                        .allowExperimentalOptions(true)
-                        .option("python.IsolateNativeModules", "true")
-                        .build()) {
-
-            if (params != null) {
-                logger.debug("Params: {}", params.toString());
-                context.getBindings("python").putMember("params", params);
+        try (final ExecutorService executor = Executors.newSingleThreadExecutor();
+                // A working context without capabilities to import packages:
+                // Context context = Context.newBuilder("python")
+                //            .sandbox(SandboxPolicy.TRUSTED)
+                //            .allowHostAccess(HostAccess.ALL).build()
+                final Context context =
+                        GraalPyResources.contextBuilder()
+                                .sandbox(SandboxPolicy.TRUSTED)
+                                .allowHostAccess(HostAccess.ALL)
+                                // The following 2 options are necessary for importing 3-rd party
+                                // libraries
+                                // that load native libraries
+                                .allowExperimentalOptions(true)
+                                .option("python.IsolateNativeModules", "true")
+                                .build()) {
+            final Future<Value> futureResult =
+                    executor.submit(() -> executeWorker(context, code, params, doc, score));
+            try {
+                return futureResult.get(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
+            } catch (TimeoutException e) {
+                context.interrupt(Duration.ZERO);
+                logger.debug("Script not evaluated within 10 seconds, interrupted.");
+                return null;
             }
-            if (doc != null) {
-                logger.debug("Doc: {}", doc.toString());
-                context.getBindings("python").putMember("doc", doc);
-            }
-            if (score != null) {
-                logger.debug("Score: {}", score);
-                context.getBindings("python").putMember("_score", score);
-            }
-            return context.eval("python", code);
         } catch (Exception e) {
             logger.error("Failed to run python code", e);
             return null;
