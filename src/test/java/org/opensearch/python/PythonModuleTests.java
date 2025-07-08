f39cce03f7f8ed3fe003e1580e9f85cb1107c3ee
Yuanchun Shen <yuanchu@amazon.com>
2025-07-08 23:04:16 +0800

    Apply spotless
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- src/test/java/org/opensearch/python/PythonModuleTests.java
+++ src/test/java/org/opensearch/python/PythonModuleTests.java
@@ -3,13 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
 package org.opensearch.python;
 
 import org.opensearch.test.OpenSearchTestCase;
--- src/yamlRestTest/java/org/opensearch/python/PythonModuleClientYamlTestSuiteIT.java
+++ src/yamlRestTest/java/org/opensearch/python/PythonModuleClientYamlTestSuiteIT.java
@@ -3,18 +3,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
 package org.opensearch.python;
 
 import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
-
 import org.opensearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.opensearch.test.rest.yaml.OpenSearchClientYamlSuiteTestCase;
 
