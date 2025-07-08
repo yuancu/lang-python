88d2cfbce6dab4de327999ce5e36c4b1d36fbc41
Yuanchun Shen <yuanchu@amazon.com>
2025-07-08 16:36:14 +0800

    Include org.graalvm.polyglot in opensearch cluster jvm args
    Signed-off-by: Yuanchun Shen <yuanchu@amazon.com>

--- src/test/java/org/opensearch/python/PythonModulePluginIT.java
+++ src/test/java/org/opensearch/python/PythonModulePluginIT.java
@@ -3,30 +3,21 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*
- * SPDX-License-Identifier: Apache-2.0
- *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
- */
 package org.opensearch.python;
 
-import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import static org.hamcrest.Matchers.containsString;
 
+import com.carrotsearch.randomizedtesting.annotations.ThreadLeakScope;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Collection;
+import java.util.Collections;
 import org.apache.hc.core5.http.ParseException;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.opensearch.client.Request;
 import org.opensearch.client.Response;
 import org.opensearch.plugins.Plugin;
 import org.opensearch.test.OpenSearchIntegTestCase;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.Collection;
-import java.util.Collections;
-
-import static org.hamcrest.Matchers.containsString;
 
 @ThreadLeakScope(ThreadLeakScope.Scope.NONE)
 @OpenSearchIntegTestCase.ClusterScope(scope = OpenSearchIntegTestCase.Scope.SUITE)
@@ -43,5 +34,15 @@ public class PythonModulePluginIT extends OpenSearchIntegTestCase {
 
         logger.info("response body: {}", body);
         assertThat(body, containsString("lang-python"));
+    }
+
+    public void testBasicExecution() throws IOException, ParseException {
+        Request request = new Request("POST", "/_scripts/python/_execute");
+        request.setJsonEntity("{\"script\":{\"source\":\"'hello ' + 'world'\"}}");
+        Response response = getRestClient().performRequest(request);
+        String body = EntityUtils.toString(response.getEntity(), StandardCharsets.UTF_8);
+
+        logger.info("response body: {}", body);
+        assertThat(body, containsString("hello world"));
     }
 }
